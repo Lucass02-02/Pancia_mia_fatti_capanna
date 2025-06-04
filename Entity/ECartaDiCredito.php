871483877e44a5003c5f50c7db05b723f9d389c1
Lucass02-02 <luave2002@gmail.com>
2025-06-04 19:29:12 +0200

    primo commit di alcune classi Entity

---
+++ Entity/ECartaDiCredito.php
@@ -0,0 +1,70 @@
+<?php
+
+class ECartaDiCredito {
+
+    //attributes
+
+    private $nominativo:
+
+    private $numero;
+
+    private $CVV;
+
+    private $datascadenza;
+
+    private $nome;
+
+    //constructor
+    public function __construct($nominativo, $numero, $CVV, $datascadenza, $nome) {
+        $this->nominativo = $nominativo;
+        $this->numero = $numero;
+        $this->CVV = $CVV;
+        $this->datascadenza = $datascadenza;
+        $this->nome = $nome;
+    }
+
+
+    //methods getters and setters
+
+    public function getNominativo() {
+        return $this->nominativo;
+    }
+
+    public function setNominativo($nominativo) {
+        $this->nominativo = $nominativo;
+    }
+
+    public function getNumero() {
+        return $this->numero;
+    }
+
+    public function setNumero($numero) {
+        $this->numero = $numero;
+    }
+
+    public function getCVV() {
+        return $this->CVV;
+    }
+
+    public function setCVV($CVV) {
+        $this->CVV = $CVV;
+    }
+
+    public function getDatascadenza() {
+        return $this->datascadenza;
+    }
+
+    public function setDatascadenza($datascadenza) {
+        $this->datascadenza = $datascadenza;
+    }
+
+    public function getNome() {
+        return $this->nome;
+    }
+
+    public function setNome($nome) {
+        $this->nome = $nome;
+    }
+
+    
+}
---
+++ Entity/ECliente.php
@@ -0,0 +1,8 @@
+<?php
+
+class ECliente extends EUtente {
+
+    //attributes
+    protected $idCliente;
+
+}
