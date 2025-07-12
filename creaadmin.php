5a4dcf4411f35c6280226292020b3e40f3e23e9d
Lucass02-02 <luave2002@gmail.com>
2025-07-13 00:21:11 +0200

    PULITO

--- creaadmin.php
+++ creaadmin.php
@@ -1,5 +1,13 @@
 <?php
+
+use AppORM\Entity\EAdmin;
+use AppORM\Services\Foundation\FPersistentManager;
+
 $password = '1'; // o la password che vuoi
 $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
-echo $hashedPassword;
+require 'bootstrap.php';
+
+$admin = new EAdmin('admin', 'admin', 'admin@gmail', 'admin1' );
+
+FPersistentManager::getInstance()->uploadObject($admin);
--- scripts/opcache_clear.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (function_exists('opcache_reset')) {
-    opcache_reset();
-    echo "OPcache resettata con successo!\n";
-} else {
-    echo "OPcache non è abilitata o la funzione opcache_reset non esiste.\n";
-}
-?>
