86fac0a14fa3369945cd9f780f5070818d477ba3
Lucass02-02 <luave2002@gmail.com>
2025-07-13 03:23:40 +0200

    BASTAAAAA

--- AppORM/Entity/EReservation.php
+++ AppORM/Entity/EReservation.php
@@ -68,7 +68,7 @@ class EReservation {
     private static $entity = EReservation::class;
 
     //constructor
-    public function __construct( $date, $hours, ?int $duration , int $peopleNum, string $nameReservation, ?string $note = null)
+    public function __construct( $date, $hours, mixed $duration , int $peopleNum, string $nameReservation, ?string $note = null)
  {
         $this->date = $date;
         $this->hours = $hours;
